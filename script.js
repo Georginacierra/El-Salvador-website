3d5ae0e52f4b4e266bd44eef78e7fff789830034
Georginacierra <georginacierra@gmail.com>
2025-06-20 16:56:04 +0200

    navbar

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ if (buttonP) {
     print();
   });
 }
+
+const burguerMenu = document.querySelector(".burguerMenu");
+const closeButton = document.querySelector(".closeButton");
+const menuMobile = document.querySelector("#menuMobile");
+
+burguerMenu.addEventListener("click", () => {
+  menuMobile.classList.remove("hideMenu");
+});
+
+closeButton.addEventListener("click", () => {
+  menuMobile.classList.add("hideMenu");
+});
+// const menuButton = document.querySelector('#menuButton');
+
+// const menu = document.querySelector('#menu');
+// const exitButton = document.querySelector('#exitButton');
+
+// menuButton.addEventListener('click', () => {
+//   menu.classList.remove('hide');
+// });
+
+// exitButton.addEventListener('click', () => {
+//   menu.classList.add('hide');
+// });
+// ```
